543bf08d980cc7fe3c7c07289d8eda79286735a7
Tô Phạm Thành Đạt <todat2207@gmail.com>
2025-07-28 14:55:36 +0700

    14.5 props.children

--- 14.0.Props_Start/src/App.jsx
+++ 14.0.Props_Start/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header.jsx";
 import { myData } from "../data.js";
 import MainContent from "./components/MainContent/MainContent.jsx";
-
+import TabButton from "./components/TabButton.jsx";
 function App() {
   // console.log(myData[0].img);
 
@@ -22,6 +22,16 @@ function App() {
             <MainContent {...myData[2]} />
             <MainContent {...myData[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+            {/* <TabButton abc="Components"></TabButton> */}
+          </menu>
         </section>
       </main>
     </>
---
+++ 14.0.Props_Start/src/components/TabButton.jsx
@@ -0,0 +1,14 @@
+export default function TabButton({ children }) {
+  return (
+    <li>
+      <button>{children}</button>
+    </li>
+  );
+}
+// export default function TabButton({ abc }) {
+//   return (
+//     <li>
+//       <button>{abc}</button>
+//     </li>
+//   );
+// }
