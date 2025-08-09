8967008de6e9d8e4be2196284c4f573e78d4f0c7
Tô Phạm Thành Đạt <todat2207@gmail.com>
2025-08-10 05:57:07 +0700

    update 14.15

--- 14.0.Props_Start/src/App.jsx
+++ 14.0.Props_Start/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 import Header from "./components/Header/Header.jsx";
-import { myData } from "../data.js";
+import { myData, EXAMPLES } from "../data.js";
 import MainContent from "./components/MainContent/MainContent.jsx";
 import TabButton from "./components/TabButton.jsx";
 function App() {
   useState();
   console.log("app dc gọi");
-  const [selectedTopic, setSelectedTopic] = useState("Click vào nút"); //update state
+  const [selectedTopic, setSelectedTopic] = useState(); //update state
   const currentHour = new Date().getHours();
   function chaoBuoi() {
     if (currentHour < 12) {
@@ -50,16 +50,34 @@ function App() {
           <h2>Examples</h2>
           {/* prettier-ignore */}
           <menu>
-            <TabButton onSelect={()=>{handleSelect(`components`);}}>Components</TabButton>
-            <TabButton onSelect={()=>{handleSelect(`jsx`);}}>JSX</TabButton>
-            <TabButton onSelect={()=>{handleSelect(`props`);}}>Props</TabButton>
-            <TabButton onSelect={()=>{handleSelect(`state`);}}>State</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === `components`}
+            onSelect={()=>{handleSelect(`components`);}}>Components</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === `jsx`}
+            onSelect={()=>{handleSelect(`jsx`);}}>JSX</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === `props`} onSelect={()=>{handleSelect(`props`);}}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === `state`} onSelect={()=>{handleSelect(`state`);}}>State</TabButton>
             <TabButton onSelect={()=>{handleSelect(chaoBuoi());}}>Chào</TabButton>    
             {/* <TabButton onSelect={createHandler('Components')}>Components</TabButton>
             <TabButton onSelect={createHandler('JSX')}>JSX</TabButton>
             <TabButton onSelect={createHandler('Props')}>Props</TabButton>
             <TabButton onSelect={createHandler('State')}>State</TabButton>  */}
           </menu>
+          {!selectedTopic ? (
+            <p>Vui lòng chọn một tab để xem examples!</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].desc}</p>
+
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+
           {selectedTopic}
         </section>
       </main>
